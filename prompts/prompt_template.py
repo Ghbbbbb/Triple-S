654802f578c1c506120798f763f0b9deab2a5042
Ghbbbbb <2323589163@qq.com>
2024-09-10 11:45:39 +0800

    first commit

--- prompts/prompt_template.py
+++ prompts/prompt_template.py
@@ -1,5 +1,4 @@
 import os
-from .tool import generate_role
 
 try:
     from langchain.prompts.chat import PromptTemplate
@@ -13,9 +12,9 @@ from langchain.prompts.chat import (
 
 class PromptLoader:
     def __init__(self, prompt_doc: str) -> None:
-        # 获取当前文件所在目录
+        #Get the current file's directory
         current_dir = os.path.dirname(__file__)
-        # 构建base目录路径
+        # Construct the base directory path
         self.dir_path = os.path.join(current_dir, prompt_doc)
 
     @property
@@ -43,110 +42,9 @@ class PromptLoader:
         return prompt
     
 
-def get_memory_conversation_template(prompt_doc: str):
-    prompt_loader = PromptLoader(prompt_doc)
-
-    B_INST, E_INST = "[INST]", "[/INST]"
-    B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
-
-    _ROBOT_PROMPT_TEMPLATE = f"""
-    {prompt_loader.sys_prompt}
-
-    {prompt_loader.sce_prompt}
-
-    {prompt_loader.pri_prompt}
-
-    """
-    _TASK_SETTINGS_PROMPT_TEMPLATE = f"""
-    Here are some rules you need to note:
-
-    {prompt_loader.settings_prompt}
-    ----------
-    """
-
-    _DEFAULT_MEMORY_CONVERSATION_TEMPLATE = B_INST + B_SYS + _ROBOT_PROMPT_TEMPLATE + _TASK_SETTINGS_PROMPT_TEMPLATE + E_SYS + """
-    Use the above context to answer the user's question and perform the user's command.
-    -----------
-    Current conversation:
-    {history}
-    Last line:
-    Human: {input}
-    You:""" + E_INST
-
-    MEMORY_CONVERSATION_TEMPLATE = PromptTemplate(
-        input_variables=["history", "input"],
-        template=_DEFAULT_MEMORY_CONVERSATION_TEMPLATE,
-    )
-
-    return MEMORY_CONVERSATION_TEMPLATE
 
 
-def get_memory_conversation_template_2(prompt_doc: str):
-    prompt_loader = PromptLoader(prompt_doc)
-
-    B_INST, E_INST = "[INST]", "[/INST]"
-    B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
-
-    _ROBOT_PROMPT_TEMPLATE = f"""
-    {prompt_loader.sys_prompt}
-
-    {prompt_loader.sce_prompt}
-
-    {prompt_loader.pri_prompt}
-
-    """
-    _TASK_SETTINGS_PROMPT_TEMPLATE = f"""
-    Here are some rules you need to note:
-
-    {prompt_loader.settings_prompt}
-    ----------
-    """
-
-    system_template = B_SYS + _ROBOT_PROMPT_TEMPLATE + _TASK_SETTINGS_PROMPT_TEMPLATE + E_SYS + """
-    Use the above context to answer the user's question and perform the user's command.
-    -----------
-    Current conversation:
-    {history}
-    """
-    # 构建初始 messages 列表，这里可以理解为是 openai 传入的 messages 参数
-    messages = [
-        SystemMessagePromptTemplate.from_template(system_template),
-        HumanMessagePromptTemplate.from_template(B_INST + '{input}' + E_INST)
-    ]
-    MEMORY_CONVERSATION_TEMPLATE_2 = ChatPromptTemplate.from_messages(messages)
-
-    return MEMORY_CONVERSATION_TEMPLATE_2
-
-
-def get_qa_template(prompt_doc: str):
-    prompt_loader = PromptLoader(prompt_doc)
-
-    B_INST, E_INST = "[INST]", "[/INST]"
-    B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
-
-    _ROBOT_PROMPT_TEMPLATE = f"""
-    {prompt_loader.sys_prompt}
-
-    {prompt_loader.sce_prompt}
-
-    {prompt_loader.pri_prompt}
-
-    """
-    _DEFAULT_QA_TEMPLATE = B_INST + B_SYS + _ROBOT_PROMPT_TEMPLATE + "{context}" + E_SYS + """
-    Use the above context to answer the user's question and perform the user's command.
-    -----------
-    Human: {question}
-    You:""" + E_INST
-
-    QA_TEMPLATE = PromptTemplate(
-        input_variables=["context", "question"],
-        template=_DEFAULT_QA_TEMPLATE,
-    )
-
-    return QA_TEMPLATE
-
-
-def get_qa_template_baichuan(prompt_doc: str,question:str,id):
+def get_qa_template_baichuan(prompt_doc: str,question:str):
     prompt_loader = PromptLoader(prompt_doc)
 
     _ROBOT_PROMPT_TEMPLATE = f"""
@@ -159,46 +57,10 @@ def get_qa_template_baichuan(prompt_doc: str,question:str,id):
     """
 
 
-    def get_summary_line(file_path, line_number):
-        with open(file_path, 'r') as file:
-            lines = file.readlines()
-            if line_number < 1 or line_number > len(lines):
-                raise ValueError("Invalid line number")
-            return lines[line_number - 1].strip()  # line_number is 1-based
-        
-    #通过调用文件生成(提前通过gpt生成)
-    SUMMARY = get_summary_line("E:/Git/Git/gitclone/MRoP/prompts/api_select_env1_level3.txt", id)
-    #通过gpt生成
-    # SUMMARY = api_select(question)
-    ROLE = generate_role(question)
-
-
-    ##################有RAG#####################
     _DEFAULT_QA_TEMPLATE_BAICHUAN1 = _ROBOT_PROMPT_TEMPLATE + "{context}" + """
 Use the above context to answer the user's question and to perform the user's command.
 -----------
 Human: {question}
-You:"""
-
-    ##################RAG+API###################
-    _DEFAULT_QA_TEMPLATE_BAICHUAN2 = _ROBOT_PROMPT_TEMPLATE + "{context}" + """
-Use the above context to answer the user's question and perform the user's command.
------------
-Human: {question}"""+"\n"+SUMMARY+"""
-You:"""
-
-    ##################RAG+ROLE###################
-    _DEFAULT_QA_TEMPLATE_BAICHUAN3 = _ROBOT_PROMPT_TEMPLATE + "{context}" + """
-Use the above context to answer the user's question and perform the user's command.
------------
-Human: {question}"""+"\n"+ROLE+"""
-You:"""
-
-    ##################RAG+API+ROLE###################
-    _DEFAULT_QA_TEMPLATE_BAICHUAN4 = _ROBOT_PROMPT_TEMPLATE + "{context}" + """
-Use the above context to answer the user's question and perform the user's command.
------------
-Human: {question}"""+"\n"+SUMMARY+"\n"+ROLE+"""
 You:"""
     
     QA_TEMPLATE_BAICHUAN = PromptTemplate(
@@ -210,8 +72,7 @@ You:"""
 
 
 
-
-def get_qa_template_baichuan_abb(prompt_doc: str):
+def get_qa_template_llama(prompt_doc: str,question:str):
     prompt_loader = PromptLoader(prompt_doc)
 
     _ROBOT_PROMPT_TEMPLATE = f"""
@@ -224,88 +85,18 @@ def get_qa_template_baichuan_abb(prompt_doc: str):
     """
 
 
-    ##################有RAG#####################
-    _DEFAULT_QA_TEMPLATE_BAICHUAN_ABB = _ROBOT_PROMPT_TEMPLATE + "{context}" + """
+    _DEFAULT_QA_TEMPLATE_LLAMA1 = "<|system|>"+_ROBOT_PROMPT_TEMPLATE + "{context}" + """
 Use the above context to answer the user's question and to perform the user's command.
 -----------
+<|user|>
 Human: {question}
+<|assistant|>
 You:"""
-    
-    QA_TEMPLATE_BAICHUAN = PromptTemplate(
+
+
+    QA_TEMPLATE_LLAMA = PromptTemplate(
         input_variables=["context", "question"],
-        template=_DEFAULT_QA_TEMPLATE_BAICHUAN_ABB,
+        template=_DEFAULT_QA_TEMPLATE_LLAMA1,
     )
 
-    return QA_TEMPLATE_BAICHUAN
-
-
-
-def get_qa_template_mistral(prompt_doc: str):
-    prompt_loader = PromptLoader(prompt_doc)
-
-    B_INST, E_INST = "[INST]", "[/INST]"
-
-    _ROBOT_PROMPT_TEMPLATE = f"""
-    {prompt_loader.sys_prompt}
-
-    {prompt_loader.sce_prompt}
-
-    {prompt_loader.pri_prompt}
-
-    """
-    _DEFAULT_QA_TEMPLATE_MISTRAL = B_INST + _ROBOT_PROMPT_TEMPLATE + "{context}" + """
-    Use the above context to answer the user's question and perform the user's command.
-    -----------
-    Human: {question}
-    You:""" + E_INST
-
-    QA_TEMPLATE_MISTRAL = PromptTemplate(
-        input_variables=["context", "question"],
-        template=_DEFAULT_QA_TEMPLATE_MISTRAL,
-    )
-
-    return QA_TEMPLATE_MISTRAL
-
-
-def get_qa_mem_template(prompt_doc: str):
-    prompt_loader = PromptLoader(prompt_doc)
-
-    B_INST, E_INST = "[INST]", "[/INST]"
-    B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
-
-    _ROBOT_PROMPT_TEMPLATE = f"""
-    {prompt_loader.sys_prompt}
-
-    {prompt_loader.sce_prompt}
-
-    {prompt_loader.pri_prompt}
-
-    """
-    _DEFAULT_QA_TEMPLATE = B_INST + B_SYS + _ROBOT_PROMPT_TEMPLATE + "{context}" + E_SYS + """
-    Use the above context to answer the user's question and perform the user's command.
-    -----------
-    {chat_history}
-    Human: {question}
-    You:""" + E_INST
-
-    QA_MEM_TEMPLATE = PromptTemplate(
-        input_variables=["context", "chat_history", "question"],
-        template=_DEFAULT_QA_TEMPLATE,
-    )
-
-    return QA_MEM_TEMPLATE
-
-
-def get_qa_template_zephyr(prompt_doc: str):
-    prompt_loader = PromptLoader(prompt_doc)
-
-    _DEFAULT_QA_TEMPLATE_ZEPHYR = '<|system|>\n' + "{context}" + """
-    Use the above context to answer the user's question and perform the user's command.
-    """ + "\n<|user|>\n{question}""" + """\n<|assistant|>"""
-
-    QA_TEMPLATE_ZEPHYR = PromptTemplate(
-        input_variables=["context", "question"],
-        template=_DEFAULT_QA_TEMPLATE_ZEPHYR,
-    )
-
-    return QA_TEMPLATE_ZEPHYR
+    return QA_TEMPLATE_LLAMA
--- prompts/tool.py
+++ prompts/tool.py
@@ -6,18 +6,13 @@ def read_content_from_file(file_path):
         content = file.read()
     return content
 
-file_api_path = 'E:/Git/Git/gitclone/MRoP/prompts/api.txt'
-specified_api_content = read_content_from_file(file_api_path)
 
-file_simp_path = 'E:/Git/Git/gitclone/MRoP/prompts/simplify2.txt'
+file_simp_path = 'E:/Git/Git/gitclone/MRoP/prompts/simplify.txt'
 specified_simp_content = read_content_from_file(file_simp_path)
 
-file_sum_path = 'E:/Git/Git/gitclone/MRoP/prompts/summary2.txt'
+file_sum_path = 'E:/Git/Git/gitclone/MRoP/prompts/summary.txt'
 specified_sum_content = read_content_from_file(file_sum_path)
 
-messages_api = [
-    {"role": "system", "content": specified_api_content},
-]
 
 messages_simple = [
     {"role": "system", "content": specified_simp_content},
@@ -27,103 +22,56 @@ messages_summary = [
     {"role": "system", "content": specified_sum_content},
 ]
 
-client = OpenAI(base_url="https://api.gpts.vin/v1",
-                api_key="sk-stvOGguPzkrr50Sf6e9d874d59534d638cE92e897956E4A2")
-#sk-ico5RYynS4XU4mi6F51b984aF71f458f8eDcFcD5BeC64d25 $5
+client = OpenAI(
+    # base_url="https://api.gpts.vin/v1",
+    )
 
 def extract_and_format(input_str):
-    # 使用正则表达式提取'{}'中的内容
+    # Use regular expressions to extract the content within '{}'
     extracted_phrases = re.findall(r'\{([^{}]+)\}', input_str)
 
-    # 将提取的内容用"then"连接
+    # Join the extracted content with "then"
     joined_phrases = ' then '.join(extracted_phrases)
 
-    # 去掉标点符号
+    # Remove punctuation
     formatted_result = re.sub(r'[^\w\s]', '', joined_phrases)
 
     return formatted_result
 
-# ######### 1.手动总结
-# while True:
-#     user_input = input("question:")
+# ######### 
+# Stage1.Simplify LLM
+def simplify(user_input:str):
+    messages_simple.append({"role": "user", "content": user_input})
+    response = client.chat.completions.create(model="gpt-3.5-turbo-0613", messages=messages_simple, stream=False,temperature=0)
+    raw_sim = response.choices[0].message.content
+    # extract_sim = extract_and_format(raw_sim)
+    return raw_sim
 
-#     def summary(user_input:str):
-#         messages_api.append({"role": "user", "content": user_input})
-#         response = client.chat.completions.create(model="gpt-3.5-turbo-0613", messages=messages_api, stream=False,temperature=0)
-#         print(response.choices[0].message.content)
-#         return response.choices[0].message.content
-#     summary(user_input)
-# 2.自动总结
-# def api_select(user_input:str):
-#     messages_api.append({"role": "user", "content": user_input})
-#     response = client.chat.completions.create(model="gpt-3.5-turbo-0613", messages=messages_api, stream=False,temperature=0)
+file_name = "test.txt"
+while True:
+    user_input = input("question:")
+    result = simplify(user_input)
+    print(result)
+    
+    # Write the results to the specified file, one line at a time
+    with open(file_name, "a", encoding="utf-8") as file:
+        file.write(result + "\n")
+
+
+
+# # # # Stage3. Summary LLM
+# def summary(user_input:str):
+#     messages_summary.append({"role": "user", "content": user_input})
+#     response = client.chat.completions.create(model="gpt-3.5-turbo-0613", messages=messages_summary, stream=False,temperature=0)
+#     print(response.choices[0].message.content)
 #     return response.choices[0].message.content
 
-###########3.手动简化
-
-# def simplify(user_input:str):
-#     messages_simple.append({"role": "user", "content": user_input})
-#     response = client.chat.completions.create(model="gpt-3.5-turbo-0613", messages=messages_simple, stream=False,temperature=0)
-#     raw_sim = response.choices[0].message.content
-#     # extract_sim = extract_and_format(raw_sim)
-#     return raw_sim
-
-# # # 指定要写入的文件名
-# file_name = "prompts\simplify_dcpd2.txt"
-
+# file_name = "test.txt"
 # while True:
 #     user_input = input("question:")
-#     result = simplify(user_input)
+#     result = summary(user_input)
 #     print(result)
     
-#     # 将结果写入指定的文件中，每次写入一行
+#     # Write the results to the specified file, one line at a time
 #     with open(file_name, "a", encoding="utf-8") as file:
 #         file.write(result + "\n")
-
-
-
-#4.规则生成
-def generate_role(input_str):
-    outputs = ["For this task, following rules may be help:"]
-    rules = []
-
-    if "facing" in input_str:
-        rules.append("Considering relative positions, if I am facing you, then my left side is your right side. If we are both facing the same direction, then my left side is still your left side.")
-    if "color" in input_str:
-        rules.append("Think about what color does those object like and then match the object with the following items in the table scene:'red_block', 'green_block', 'green_block', 'yellow_block', 'white_mug' and 'black_mug'.")
-    if "left" in input_str:
-        rules.append("If current position is cur_pos, then move 10cm left --> ```cur_pos + np.array([0, 0.1, 0])```")
-    if "right" in input_str:
-        rules.append("If current position is cur_pos, then move 10cm right --> ```cur_pos + np.array([0, -0.1, 0])```")
-    if "forward" in input_str or "front" in input_str:
-        rules.append("If current position is cur_pos, then move 10cm forward --> ```cur_pos + np.array([0.1, 0, 0])```")
-    if "behind" in input_str or "backward" in input_str:
-        rules.append("If current position is cur_pos, then move 10cm backwards --> ```cur_pos + np.array([-0.1, 0, 0])```")
-    if "over" in input_str or "above" in input_str:
-        rules.append("If current position is cur_pos, then move 10cm above --> ```cur_pos + np.array([0, 0, 0.1])```")
-    if "farthest" in input_str or "closest" in input_str:
-        rules.append("First to get the obj pose use ```pri.get_obj_pose()```, then calculate the distance between the objects using ```np.linalg.norm()```")
-    if not rules:
-        return ""
-    
-    for idx, rule in enumerate(rules, 1):
-        outputs.append(f"{idx}.{rule}")
-    
-    return "\n".join(outputs)
-
-
-# # 2.手动更新
-# while True:
-#     user_input = input("question:")
-
-#     def summary(user_input:str):
-#         messages_api.append({"role": "user", "content": user_input})
-#         response = client.chat.completions.create(model="gpt-4-turbo", messages=messages_summary, stream=False,temperature=0)
-#         print(response.choices[0].message.content)
-#         return response.choices[0].message.content
-#     summary(user_input)
-# # 2.自动更新
-# def api_select(user_input:str):
-#     messages_api.append({"role": "user", "content": user_input})
-#     response = client.chat.completions.create(model="gpt-3.5-turbo-0613", messages=messages_api, stream=False,temperature=0)
-#     return response.choices[0].message.content
